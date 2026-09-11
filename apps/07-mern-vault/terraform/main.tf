4e42d54acffc2917390a7d0dbf578ad37bf92515
Christian Renaud <christianrenaud@gmail.com>
2026-09-11 09:46:37 -0400

    fix(demo-app-07): fix EKS module input from cluster_addons to addons for v21 module compatibility

--- apps/07-mern-vault/terraform/main.tf
+++ apps/07-mern-vault/terraform/main.tf
@@ -60,7 +60,7 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  cluster_addons = {
+  addons = {
     coredns    = {}
     kube-proxy = {}
     vpc-cni = {
